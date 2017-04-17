d54031b606ab2432c21a3f2c07d6874a18c387bf
dabo02 <dabo_02@live.com>
2017-04-17 17:24:12 -0400

    added resume and fixed stay in touch section

--- Portfolio.py
+++ Portfolio.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Message, Mail
-
+import socket as s
+import os
 app = Flask(__name__)
-app.config.update(DEBUG = True,
-    MAIL_SERVER = 'smtp.live.com',
-    MAIL_PORT = 587,
-    MAIL_USE_TLS = True,
-    MAIL_USE_SSL = False,
-    MAIL_USERNAME = 'dabo_02@live.com',
-    MAIL_PASSWORD = 'nomeronques2',
-    DEFAULT_MAIL_SENDER = 'dabo_02@live.com')
+host = s.gethostname()
 mail = Mail(app)
+if 'liveconsole' not in host:
+    app.config.update(DEBUG = True,
+        MAIL_SERVER = 'smtp.live.com',
+        MAIL_PORT = 587,
+        MAIL_USE_TLS = True,
+        MAIL_USE_SSL = False,
+        MAIL_USERNAME = os.environ.get('MAIL_USERNAME'),
+        MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD'),
+        DEFAULT_MAIL_SENDER = os.environ.get('MAIL_USERNAME'))
+else:
+    app.config.update(DEBUG = True,
+                      MAIL_SERVER = 'smtp.gmail.com',
+                      MAIL_PORT = 587,
+                      MAIL_USE_TLS = True,
+                      MAIL_USE_SSL = False,
+                      MAIL_USERNAME = os.environ.get('MAIL_USERNAME'),
+                      MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD'),
+                      DEFAULT_MAIL_SENDER = os.environ.get('MAIL_USERNAME'))
+
+
 @app.route('/')
 def my_site():
     return render_template('index.html')
@@ -22,11 +36,16 @@ def send_email():
     email = request.form['email']
     subj = request.form['subject']
     msg = request.form['message']
-    mail_to_send = Message(subject=subj,
-                           recipients=['dabo021213@gmail.com'],
-                           body=msg + ' From: ' + name + ' with email: ' + email,
-                           sender=(name, email))
-    # Send the message via our own SMTP server.
+    if 'liveconsole' not in host:
+        mail_to_send = Message(subject=subj,
+                               recipients=['dabo021213@gmail.com'],
+                               body=msg + ' From: ' + name + ' with email: ' + email,
+                               sender=(name, email))
+    else:
+        mail_to_send = Message(subject=subj,
+                               recipients=['dabo_02@live.com'],
+                               body=msg + ' From: ' + name + ' with email: ' + email,
+                               sender=(name, email))
     mail.send(mail_to_send)
     return redirect('/', code=301)
 
@@ -36,7 +55,6 @@ def video_chat():
     return render_template('video_chat.html')
 
 if __name__ == '__main__':
-    import socket as s
-    if 'liveconsole' not in s.gethostname():
+    if 'liveconsole' not in host:
         mail.init_app(app)
         app.run()
